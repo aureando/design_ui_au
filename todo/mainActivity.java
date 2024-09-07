75d4fb3d2870e56d8f65b25437776ff23c3089fd
aureando <raureando@gmail.com>
2024-09-07 12:44:51 +0300

    Design UI To-doList

---
+++ todo/mainActivity.java
@@ -0,0 +1,188 @@
+package com.my.newproject2;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.*;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.*;
+import org.json.*;
+
+public class MainActivity extends AppCompatActivity {
+	
+	private HashMap<String, Object> m = new HashMap<>();
+	
+	private ArrayList<HashMap<String, Object>> todo = new ArrayList<>();
+	
+	private LinearLayout header;
+	private LinearLayout body;
+	private TextView textview1;
+	private RecyclerView recyclerview1;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.main);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		header = findViewById(R.id.header);
+		body = findViewById(R.id.body);
+		textview1 = findViewById(R.id.textview1);
+		recyclerview1 = findViewById(R.id.recyclerview1);
+	}
+	
+	private void initializeLogic() {
+		for(int _repeat18 = 0; _repeat18 < (int)(15); _repeat18++) {
+			{
+				HashMap<String, Object> _item = new HashMap<>();
+				_item.put("title", "");
+				todo.add(_item);
+			}
+			
+			{
+				HashMap<String, Object> _item = new HashMap<>();
+				_item.put("description", "");
+				todo.add(_item);
+			}
+			
+		}
+		recyclerview1.setAdapter(new Recyclerview1Adapter(todo));
+		recyclerview1.setLayoutManager(new LinearLayoutManager(this));
+	}
+	
+	public class Recyclerview1Adapter extends RecyclerView.Adapter<Recyclerview1Adapter.ViewHolder> {
+		
+		ArrayList<HashMap<String, Object>> _data;
+		
+		public Recyclerview1Adapter(ArrayList<HashMap<String, Object>> _arr) {
+			_data = _arr;
+		}
+		
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			LayoutInflater _inflater = getLayoutInflater();
+			View _v = _inflater.inflate(R.layout.listui, null);
+			RecyclerView.LayoutParams _lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			_v.setLayoutParams(_lp);
+			return new ViewHolder(_v);
+		}
+		
+		@Override
+		public void onBindViewHolder(ViewHolder _holder, final int _position) {
+			View _view = _holder.itemView;
+			
+			final LinearLayout body_liste = _view.findViewById(R.id.body_liste);
+			final LinearLayout linear2 = _view.findViewById(R.id.linear2);
+			final LinearLayout linear3 = _view.findViewById(R.id.linear3);
+			final LinearLayout linear4 = _view.findViewById(R.id.linear4);
+			final LinearLayout dot = _view.findViewById(R.id.dot);
+			final TextView title_do = _view.findViewById(R.id.title_do);
+			final TextView desc_do = _view.findViewById(R.id.desc_do);
+			final ImageView del = _view.findViewById(R.id.del);
+			
+			body_liste.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)25, 0xFF673AB7));
+			dot.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)60, 0xFFEDE7F6));
+			if (todo.get((int)_position).containsKey("title") && todo.get((int)_position).containsKey("description")) {
+				title_do.setText(_data.get((int)_position).get("title").toString());
+				desc_do.setText(_data.get((int)_position).get("description").toString());
+			}
+			else {
+				title_do.setText("Coding");
+				desc_do.setText("Apprendre la langage JAVA");
+			}
+		}
+		
+		@Override
+		public int getItemCount() {
+			return _data.size();
+		}
+		
+		public class ViewHolder extends RecyclerView.ViewHolder {
+			public ViewHolder(View v) {
+				super(v);
+			}
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
